da33dbff938505effff3b31bbe6666aa1f6398af
Shane Moo <obshane96@gmail.com>
2023-10-09 19:06:21 -0400

    Change zsh configuration

--- .zsh/oh-my-zsh.zsh
+++ .zsh/oh-my-zsh.zsh
@@ -5,8 +5,6 @@ ZSH=$HOME/.oh-my-zsh
 
 ZSH_THEME="blinks"
 
-ZSH_TMUX_AUTOSTART=true
-
 plugins=(git gpg-agent ripgrep tmux colored-man-pages fzf vscode gh aws kubectl helm docker)
 
 source $ZSH/oh-my-zsh.sh
